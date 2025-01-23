c1ca42fd5170146bcf2c1f8da98339a5a46dd6ba
Ahmidserhan <halonahmidserhan@gmail.com>
2025-01-23 16:47:02 +0800

    first commit - add name and pic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 const Hero = () => {
   return (
     <section id="home" className="hero">
-      <h1>Hi, I'm Ahmidserhan Halon</h1>
-      <p>Frontend Developer | Wannabe Programmer</p>
-      <button className="cta-button">View My Work</button>
+      <div className="hero-content">
+        <h1>Hi, I'm Ahmidserhan Halon</h1>
+        <p>Frontend Developer | Wannabe Programmer</p>
+        <button className="cta-button">View My Work</button>
+      </div>
+      <div className="hero-image">
+        <img src="./src/assets/images/labrador.jpg" alt="Golden Retriever" />
+      </div>
     </section>
   );
 };
